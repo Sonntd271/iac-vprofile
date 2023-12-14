2239c39f14789fe8df57605e8df891b57e7ec4aa
Sonntd271 <itssonntd271@gmail.com>
2023-12-14 16:34:18 +0100

    More testing

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
+  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.7.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
